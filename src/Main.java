c1f736fdfb817db0a478841f41f4d94242f54e38
bobriki <bobriki@LAPTOP-IV97264L>
2021-11-03 18:04:25 +0300

    Create class MoneySaver

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,5 @@
- public class Main {
-	 public static void main (String[] args) {
-		 System.out.println("I'm just checking how it's working");
-		 System.out.println("I'm just checking how it's working");
-	 }
+public class Main {
+    public static void main(String[] args) {
+        new MoneySaver().main();
+    }
 }
